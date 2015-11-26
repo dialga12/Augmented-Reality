39d7e002c1867f96708b3d4b226458e7289b66bc
lomejormundo <lomejormundo@hotmail.com>
2015-11-25 22:57:27 -0500

    Commit #11: - Se sube una interfaz estable. - Se suben algunos scripts desarrollados en el MotorConocimiento, funcionalidades para nuestros objetos. - Se creo una nueva escena llamada Interfaz. - Se agregaron botones para la activación o desactivación de recursos. - Se agrego la funcionalidad de poder cambiar entre escenas.

---
+++ Assets/MotorConocimiento/Camara.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camara : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnMouseDown(){
+		// this object was clicked - do something
+		Application.LoadLevel("PrimeraEscena");
+	}
+		
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/MotorConocimiento/Salir.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Salir : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnMouseDown(){
+		// this object was clicked - do something
+		Application.Quit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
