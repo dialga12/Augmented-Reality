792c5ea22f7c8bd1cff0f20b751ad85ac5ab463a
lomejormundo <lomejormundo@hotmail.com>
2015-11-26 12:09:24 -0500

    Commit #13: - Se vuelve a subir el proyecto, - Incluyendo el ejecutable - También se hicieron algunas modificaciones en la interfaz.

---
+++ SantuariosAndinos/Assets/MotorConocimiento/Imagenes.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Imagenes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {	
+		cont = 0;
+		imagenes = GameObject.FindGameObjectWithTag("Imagenes");
+	}
+	public int cont;
+	public GameObject imagenes;
+
+	void OnMouseDown(){
+		// this object was clicked - do something
+		if (cont % 2 == 0) {
+			imagenes.SetActive (false);
+			cont++;
+		}
+		else {
+			imagenes.SetActive (true);
+		cont++;
+	}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ SantuariosAndinos/Assets/MotorConocimiento/Regresar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Regresar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnMouseDown(){
+		// this object was clicked - do something
+		Application.LoadLevel("Interfaz");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
